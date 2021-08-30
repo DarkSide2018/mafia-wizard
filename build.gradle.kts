fad36a450771dd687932ace4587fb898bc337143
DarkSide <romangaponov@gmail.com>
2021-08-30 15:03:07 +0300

    change java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    kotlin("jvm")
     id("org.springframework.boot") version "2.5.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("java")
+    kotlin("jvm")
     kotlin("plugin.spring") version "1.5.21"
 }
 val springBootVersion: String by project

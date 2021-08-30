2281834664d30c223484ebc036bd4df85a728d40
DarkSide <romangaponov@gmail.com>
2021-08-30 14:06:23 +0300

    fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,6 @@ tasks.withType<Jar> {
     manifest {
         attributes["Main-Class"] = "mafia.wizard.AppGeneralKt"
     }
-    from(sourceSets.main.get().output)
-
     dependsOn(configurations.runtimeClasspath)
     from({
         configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }

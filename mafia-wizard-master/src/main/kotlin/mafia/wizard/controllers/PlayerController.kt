eebec560a7fc31cd66b64e4704d1ad2ebe19645e
DarkSide <romangaponov@gmail.com>
2021-09-03 21:37:40 +0300

    create controller

--- mafia-wizard-master/src/main/kotlin/mafia/wizard/controllers/PlayerController.kt
+++ mafia-wizard-master/src/main/kotlin/mafia/wizard/controllers/PlayerController.kt
@@ -1,9 +1,12 @@
 package mafia.wizard.controllers
 
+import mafia.wizard.openapi.models.*
 import mafia.wizard.services.PlayerService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import javax.websocket.server.PathParam
 
 @RestController
 @CrossOrigin
@@ -14,19 +17,23 @@ class PlayerController(
     @Autowired
     lateinit var playerService: PlayerService
     @GetMapping
-    fun getByNickname(nickName: String){
+    fun getByNickname(@PathParam("nickname") nickName: String):ResponseEntity<ReadPlayerResponse>{
 
+        return ResponseEntity.ok().build()
     }
     @PostMapping
-    fun createPlayer(){
-
+    fun createPlayer(@RequestBody createPlayerRequest: CreatePlayerRequest):ResponseEntity<CreatePlayerResponse>{
+        
+        return ResponseEntity.ok().build()
     }
     @PutMapping
-    fun updatePlayer(){
+    fun updatePlayer(@RequestBody updatePlayerRequest: UpdatePlayerRequest):ResponseEntity<UpdatePlayerResponse>{
 
+        return ResponseEntity.ok().build()
     }
     @DeleteMapping
-    fun deletePlayer(uuid: UUID){
+    fun deletePlayer(@PathParam("uuid") uuid: UUID):ResponseEntity<DeletePlayerResponse>{
 
+        return ResponseEntity.ok().build()
     }
 }
